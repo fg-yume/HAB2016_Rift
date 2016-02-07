4af94216199c06ec6ec4ed1964b174a24017b4c4
StringNotFound <lkasser8@gmail.com>
2016-02-07 06:45:16 -0600

    Added collision detection for map

--- HAB2016_OculusGame/Assets/Movement.cs
+++ HAB2016_OculusGame/Assets/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     public float turnSpeed = 50f;
     private float DELAY_TIME = 0.3f;
     private Vector3 bikedir;
+
+    private int[] m_lastPos;
     int direction;
     double turnDelay;
     // Use this for initialization
@@ -18,6 +20,8 @@ public class Movement : MonoBehaviour
         //bikedir = new Vector3(0, 0, 1);
         direction = 90;
         turnDelay = -1;
+
+        m_lastPos = new int[] { -1, -1 };
     }
 
     // Update is called once per frame
@@ -29,6 +33,67 @@ public class Movement : MonoBehaviour
         transform.Translate(new Vector3(-1, 0, 0) * moveSpeed * Time.deltaTime);
 
         //transform.position += Vector3.forward * Time.deltaTime;
+
+        collisionCheck();
+    }
+
+    private void die()
+    {
+        Debug.Log("You died! :]");
+    }
+
+    public void collisionCheck()
+    {
+        // check if current square is taken
+        int[] currPos = getRelativeMapLocation();
+
+        // compare curr position vs last position
+        if (m_lastPos[0] == -1)
+        {
+
+            m_lastPos[0] = currPos[0];
+            m_lastPos[1] = currPos[1];
+
+            return;
+        }
+
+        // check for collision here!
+        if ( Main.m_grid[ currPos[0], currPos[1] ] == true )
+        {
+            // we done goofed
+            die();
+        }
+
+        if ( m_lastPos[0] == currPos[0] &&
+            m_lastPos[1] == currPos[1] )
+        {
+            // Don't update things, we haven't moved out of previous position yet
+            return;
+        }
+
+        //Debug.Log("New position: [" + currPos[0] + "," + currPos[1] + "]");
+
+        // update last position
+        Main.m_grid[m_lastPos[0], m_lastPos[1]] = true; // ;-;
+
+        m_lastPos[0] = currPos[0];
+        m_lastPos[1] = currPos[1];
+
+        //Debug.Log("test on Main: [" + m_last)
+    }
+    
+    private int[] getRelativeMapLocation()
+    {
+        // 1000x1000 top-left: -200, 200, bottom-right: 200, -200
+        // units of .2
+        //int mapLocation[2];
+
+        int gridPosZ = (int) (transform.position.z / .2);
+        int gridPosX = (int)(transform.position.x / .2);
+
+        //Debug.Log(gridPosX + "," + gridPosZ);
+
+        return new int []{ gridPosX, gridPosZ };
     }
 
     void turnButton2()
--- HAB2016_OculusGame/Assets/Scripts/Main.cs
+++ HAB2016_OculusGame/Assets/Scripts/Main.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
     private int TARGET_FRAME_RATE = 90;
+    private int m_width = 1000;
+
+    public static bool[,] m_grid;
 
     void Awake()
     {
@@ -13,7 +16,23 @@ public class Main : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("test");
-	}
+
+        m_grid = new bool[m_width, m_width];
+        for (int i = m_width - 1; i >= 0; --i)
+        {
+            for (int j = m_width - 1; j >= 0; --j)
+            {
+                m_grid[i, j] = false;
+            }
+        }
+        for (int i = m_width - 1; i >= 0; --i)
+        {
+            m_grid[i, 0] = true;
+            m_grid[0, i] = true;
+            m_grid[i, m_width - 1] = true;
+            m_grid[m_width - 1, i] = true;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
