20b02eb03bf543d8e2513b1baf642e375c355008
PrestonPerriott <preston.ap94@gmail.com>
2016-02-06 19:22:51 -0500

    Basic Camera Controls & movement

---
+++ HAB2016_OculusGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float movespeed;
+	//public float turnspeed = 50f;
+	private Vector3 Cube;
+
+	void Start () { 
+		gameObject.SetActive(true);
+		Cube = Vector3.forward;
+	}
+
+
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			movespeed = 10;
+			transform.Translate (new Vector3 (0, 0, movespeed) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			movespeed = -10;
+			transform.Translate (new Vector3 (0, 0, movespeed) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			movespeed = -10;
+			transform.Translate (new Vector3 (movespeed ,0, 0 ) * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			movespeed = 10;
+			transform.Translate (new Vector3 (movespeed ,0, 0 ) * Time.deltaTime);
+	
+		}
+		//transform.Translate (Cube * movespeed * Time.deltaTime);
+	}
+
+}
